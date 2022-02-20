669e603f5f71dc1f092642031a359fb4990d7bef
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-02-20 01:41:03 +0100

    Realizada instalación de express

---
+++ server/install_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "→ Realizando instalación de 'npm'"
+sudo apt install npm
+echo "→ Instalación de 'npm' finalizada"
+
+echo "→ Realizando instalación de 'express' mediante npm"
+npm i express
+echo "→ Instalación de 'express' finalizada"
