b4391044e968831702328baa6793fdef9922939b
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-10 18:32:33 +0100

    Prueba controlador

--- Modelo/prueba.py
+++ Modelo/prueba.py
@@ -1,9 +1,46 @@
+"""
 import requests
 
-url = "http://f0bc-34-86-152-52.ngrok.io/inferencia"
+
+url = "http://1ebc-34-121-126-47.ngrok.io/inferencia"
 usuario = {
     "question": "A cat sat on a mat",
 }
-respuesta = requests.post(url, json=usuario)
+#respuesta = requests.post(url, json=usuario)
 
-print(respuesta.text)
+#print(respuesta.text)
+
+requests.get("http://fd31-34-121-126-47.ngrok.io/shutdown")
+"""
+
+
+import requests
+
+from flask import Flask, request
+
+app = Flask(__name__) 
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+  req = request.get_json(silent=True, force=True)
+  query_result = req.get('queryResult')
+  if query_result.get('displayName') == 'Prueba':
+    url = "http://50e4-35-238-140-113.ngrok.io/inferencia"
+    question = query_result.get('queryText')
+    usuario = {
+        "question": question,
+    }
+    fulfillmentText = requests.post(url, json=usuario)
+
+  return {
+    "fulfillmentText": fulfillmentText,
+    "source": "webhookdata"
+  }
+
+  # return response 
+
+@app.route("/") 
+def home(): 
+  return "Servidor que ejecuta el modelo de GPT-J"
+
+app.run()
