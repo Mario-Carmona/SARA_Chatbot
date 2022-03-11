c04ea548d3aaea3488b085e1828d4400bd841876
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-11 13:14:39 +0100

    Arreglado if intent

--- app/main.py
+++ app/main.py
@@ -23,26 +23,19 @@ app = Flask(__name__)
 @app.route("/webhook", methods=["POST"])
 def webhook():
   req = request.get_json(silent=True, force=True)
-  query_result = req.get('queryResult')
-  """
-  if query_result.get('displayName') == 'Prueba':
+  
+  query_result = req.get("queryResult")
+  intent = query_result.get("intent").get("displayName")
+
+  if intent == "Prueba":
     url = "http://4416-35-184-60-16.ngrok.io/inferencia"
-    question = query_result.get('queryText')
+    question = query_result.get("queryText")
     usuario = {
         "question": question,
     }
     fulfillmentText = requests.post(url, json=usuario)
-    fulfillmentText = "Dentro"
   else:
-    fulfillmentText = query_result.get('queryText')
-  """
-
-  url = "http://c476-34-69-136-80.ngrok.io/inferencia"
-  question = query_result.get('queryText')
-  usuario = {
-      "question": question,
-  }
-  fulfillmentText = requests.post(url, json=usuario)
+    fulfillmentText = "Sin respuesta"
 
   return {
     "fulfillmentText": fulfillmentText,
