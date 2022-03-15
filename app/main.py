981f768fafe2c2635b01313b733d99da66e033cc
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-15 18:48:00 +0100

    Prueba

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import os
 from flask import Flask, request, send_file
 
 
@@ -38,4 +39,5 @@ def webhook():
   }
 
 if __name__ == "__main__":
-    app.run()
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=False, host="0.0.0.0", port=port)
