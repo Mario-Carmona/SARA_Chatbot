c41db71f581bf497f415dd7f1345beddbe63ca70
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-15 15:02:18 +0100

    Prueba

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-import views
+@app.route("/") 
+def home(): 
+  return "<h1>Bienvenido al servidor</h1>"
 
 if __name__ == "__main__":
     app.run()
-    
