deb4516e82f152465ddfa1ba50500dabe72520d0
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-16 01:24:05 +0100

    Clase Server

--- app/main.py
+++ app/main.py
@@ -1,47 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from distutils.log import debug
 import requests
 import os
 from flask import Flask, request, send_file
 
 
-app = Flask(__name__)
+class Server:
+    app = Flask(__name__)
 
-@app.route("/") 
-def home(): 
-  return "<h1>Bienvenido al servidor</h1>"
+    def __init__(self, host, port, debug):
+        self.host = host
+        self.port = port
+        self.debug = debug
 
-@app.route("/wakeup", methods=["GET"])
-def wakeup():
-    return "Server ON"
+    def run(self):
+        Server.app.run(debug=self.debug, host=self.host, port=self.port)
 
-@app.route("/interface", methods=["GET", "POST"])
-def interface():
-    return send_file('./templates/interface.html')
+    @app.route("/") 
+    def home(): 
+        return "<h1>Bienvenido al servidor</h1>"
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
-  req = request.get_json(silent=True, force=True)
-  
-  query_result = req.get("queryResult")
-  intent = query_result.get("intent").get("displayName")
+    @app.route("/wakeup", methods=["GET"])
+    def wakeup():
+        return "Server ON"
 
-  if intent == "Prueba":
-    url = "http://4416-35-184-60-16.ngrok.io/inferencia"
-    question = query_result.get("queryText")
-    usuario = {
-        "question": question,
-    }
-    fulfillmentText = requests.post(url, json=usuario)
-  else:
-    fulfillmentText = "Sin respuesta"
+    @app.route("/interface", methods=["GET", "POST"])
+    def interface():
+        return send_file('./templates/interface.html')
 
-  return {
-    "fulfillmentText": fulfillmentText,
-    "source": "webhookdata"
-  }
+    @app.route("/webhook", methods=["POST"])
+    def webhook():
+        req = request.get_json(silent=True, force=True)
+        
+        query_result = req.get("queryResult")
+        intent = query_result.get("intent").get("displayName")
+
+        if intent == "Prueba":
+            url = "http://4416-35-184-60-16.ngrok.io/inferencia"
+            question = query_result.get("queryText")
+            usuario = {
+                "question": question,
+            }
+            fulfillmentText = requests.post(url, json=usuario)
+        else:
+            fulfillmentText = "Sin respuesta"
+
+        return {
+        "fulfillmentText": fulfillmentText,
+        "source": "webhookdata"
+        }
 
 if __name__ == "__main__":
+
+    host = "0.0.0.0"
     port = os.environ.get("PORT", 5000)
-    app.run(debug=False, host="0.0.0.0", port=port)
+    debug = False
+
+    server = Server(host, port, debug)
+
+    server.run()
