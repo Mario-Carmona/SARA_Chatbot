66de9b600f96e1df13bec06044fb00c99a922da2
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-24 00:13:29 +0100

    Añadido menu lateral

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -4,3 +4,16 @@ buttonSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark_mode');
     buttonSwitch.classList.toggle('active');
 });
+
+const buttonSidebar = document.querySelector('#button_sidebar');
+const sidebar = document.querySelector('#sidebar');
+
+buttonSidebar.addEventListener('click', () => {
+    sidebar.classList.toggle('deploy');
+});
+
+window.addEventListener('resize', () => {
+    if (sidebar.classList.contains('deploy'))
+        if (document.documentElement.clientWidth > 750)
+            sidebar.classList.toggle('deploy');
+});
