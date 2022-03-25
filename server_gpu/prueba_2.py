ef854bf7da1286e1446c0e527688c75249b1be6e
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-25 13:51:12 +0100

    Cambio

--- server_gpu/prueba_2.py
+++ server_gpu/prueba_2.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoConfig, GPTJForCausalLM, pipeline
+from transformers import AutoTokenizer, AutoConfig, GPTJForCausalLM
 from transformers.deepspeed import HfDeepSpeedConfig
 import deepspeed
 import os
@@ -18,23 +18,30 @@ deepspeed.init_distributed()
 
 model_name = "../EleutherAI/gpt-j-6B"
 
-generator = pipeline('text-generation', model=model_name, tokenizer=model_name,
-                     device=local_rank)
-
+model = GPTJForCausalLM.from_pretrained(model_name, revision=torch.float16, low_cpu_mem_usage=True)
 
 with torch.no_grad():
-    generator.model = deepspeed.init_inference(generator.model,
-                                            mp_size=world_size,
-                                            dtype=torch.float16,
-                                            replace_method='auto',
-					                        replace_with_kernel_inject=True)
+    ds_engine = deepspeed.init_inference(model,
+                                        mp_size=world_size,
+                                        dtype=torch.float16,
+                                        replace_method='auto',
+					                    replace_with_kernel_inject=True)
+
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 import time
 
 inicio = time.time()
-string = generator("DeepSpeed is", do_sample=True, min_length=50)
+inputs = tokenizer.encode("DeepSpeed is", return_tensors="pt").to(device=local_rank)
+
+with torch.no_grad():
+    outputs = ds_engine.module.generate(inputs, do_sample=True, max_length = 100, min_length=100, synced_gpus=True)
+
+text_out = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
 fin = time.time()
 
 if not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0:
-    print(string)
+    print(text_out)
 print(fin-inicio)
