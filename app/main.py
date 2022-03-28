10e841d2c7efab6b755c87b493d68192fddb2cb0
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-28 20:17:55 +0200

    Cambio

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ import uvicorn
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="./static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="./templates")    
 
