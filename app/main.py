e4ee9116db9c58fc29188b98ce882f69faa6a9a2
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-29 00:23:37 +0200

    Cambio

--- app/main.py
+++ app/main.py
@@ -47,7 +47,7 @@ def wakeup():
     return "Server ON"
 
 @app.post("/webhook")
-def webhook(request: Request):
+async def webhook(request: Request):
     req = request.get_json(silent=True, force=True)
     
     query_result = req.get("queryResult")
