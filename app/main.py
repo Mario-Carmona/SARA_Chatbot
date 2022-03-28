5de7e100c4c6192cc92e0bff6847fcf8edfa20b9
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-28 20:58:43 +0200

    Cambios

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import json
 import requests
 import os
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, PlainTextResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
@@ -22,82 +22,82 @@ templates = Jinja2Templates(directory=str(BASE_PATH/"templates"))
 async def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
-    """
-    @app.route("/wakeup", methods=["GET"])
-    def wakeup():
-        return "Server ON"
+@app.get("/chatbot")
+def chatbot(request: Request):
+    return templates.TemplateResponse("chatbot.html", {"request": request})
 
-    @app.route("/interface", methods=["GET", "POST"])
-    def interface():
-        return send_file('./templates/interface.html')
+@app.get("/interface")
+def interface(request: Request):
+    return templates.TemplateResponse("interface.html", {"request": request})
 
-    @app.route("/chatbot", methods=["GET", "POST"])
-    def chatbot():
-        return send_file('./templates/chatbot.html')
+@app.get("/wakeup", response_class=PlainTextResponse)
+def wakeup():
+    return "Server ON"
 
-    @app.route("/webhook", methods=["POST"])
-    def webhook():
-        req = request.get_json(silent=True, force=True)
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    
+    query_result = req.get("queryResult")
+    intent = query_result.get("intent").get("displayName")
+
+    if intent == "Welcome":
+        outputContexts = query_result.get("outputContexts")
+        name = outputContexts[0].get("name")
+        session_id = req.get("responseId")
         
-        query_result = req.get("queryResult")
-        intent = query_result.get("intent").get("displayName")
-
-        if intent == "Welcome":
-            outputContexts = query_result.get("outputContexts")
-            name = outputContexts[0].get("name")
-            session_id = req.get("responseId")
-            
-            webhookResponse = {
-                "outputContexts": [
-                    {
-                        "name": name,
-                        "lifespanCount": 5,
-                        "parameters": {
-                            "session_id": session_id
-                        }
+        webhookResponse = {
+            "outputContexts": [
+                {
+                    "name": name,
+                    "lifespanCount": 5,
+                    "parameters": {
+                        "session_id": session_id
                     }
-                ]
-            }
-        elif intent == "Talk":
-            url = config["inference_url"]
-            question = query_result.get("queryText")
-            query_json = {
-                "question": question,
-            }
-            answer = requests.post(url, json=query_json)
+                }
+            ]
+        }
+    elif intent == "Talk":
+        url = config["inference_url"]
+        question = query_result.get("queryText")
+        query_json = {
+            "question": question,
+        }
+        answer = requests.post(url, json=query_json)
 
-            outputContexts = query_result.get("outputContexts")
+        outputContexts = query_result.get("outputContexts")
 
-            webhookResponse = {
-                "fulfillmentMessages": [
-                    {
-                        "text": {
-                            "text": [
-                                answer
-                            ]
-                        }
+        webhookResponse = {
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [
+                            answer
+                        ]
                     }
-                ],
-                "outputContexts": outputContexts
-            }
-        elif intent == "Goodbye":
-            # Implementar guardado del historial
-            pass
-        else:
-            webhookResponse = {
-                "fulfillmentMessages": [
-                    {
-                        "text": {
-                            "text": [
-                                "Sin respuesta"
-                            ]
-                        }
+                }
+            ],
+            "outputContexts": outputContexts
+        }
+    elif intent == "Goodbye":
+        # Implementar guardado del historial
+        pass
+    else:
+        webhookResponse = {
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [
+                            "Sin respuesta"
+                        ]
                     }
-                ]
-            }
+                }
+            ]
+        }
 
-        return webhookResponse
-    """
+    return webhookResponse
+
+
 
 if __name__ == "__main__":
 
