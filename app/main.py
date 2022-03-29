29e56221671d176864b8928c41a76292912eb6c9
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-30 00:56:47 +0200

    Cambio

--- app/main.py
+++ app/main.py
@@ -149,7 +149,6 @@ def add_process_time_header(request: Request, call_next):
     start_time = time()
     response = call_next(request)
     process_time = time() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
     return response
 
 @app.get("/", response_class=HTMLResponse) 
