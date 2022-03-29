6a4b3d00c7961a3aef39480bd2ae35095cb27111
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-03-29 20:49:16 +0200

    Cambio

--- app/main.py
+++ app/main.py
@@ -76,7 +76,7 @@ def make_response_deduct_talk(request: Dict):
 
     outputContexts = request.get("queryResult").get("outputContexts")
 
-    if(outputContexts[POS_EDAD].key("parameters") == None):
+    if("parameters" in outputContexts[POS_EDAD].keys()):
         return make_response_deduct(request)
     else:
         return make_response_talk(request)
