be9b620180d81f774659cbb4deff54d51b9eb937
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-04 00:21:14 +0200

    Nuevo create_dataset

--- server_gpu/create_dataset.py
+++ server_gpu/create_dataset.py
@@ -2,32 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-from cmath import nan
 import pandas as pd
 import sys
+import os
 
-
-def generarContexto(dataset):
-    context = []
-    nombre = dataset["Nombre"].tolist()
-    nombre_alter = dataset["Nombre_Alternativo"].tolist()
-    definicion = dataset["Definición"].tolist()
-
-    for i in range(len(nombre)):
-        aux = nombre[i]
-        if str(nombre_alter[i]) != "nan":
-            aux += ". " + nombre_alter[i]
-        aux += ". " + definicion[i]
-
-        context.append(aux)
-
-    dataset = dataset.drop(
-        ["Nombre", "Nombre_Alternativo", "Definición"], 
-        axis=1
-    )
-    dataset["Contexto"] = context
-
-    return dataset
 
 def obtenerTrainDataset(dataset, train_split):
     train_dataset = dataset.sample(
@@ -52,26 +30,34 @@ def obtenerValidationDataset(dataset, train_dataset):
 
     return validation_dataset
 
+def obtenerSourceTarget(dataset):
+    dataset["Source"] = dataset["Nombre"]
+    dataset["Source"] += ". "
+    for i, nom in enumerate(dataset["Nombre_Alternativo"]):
+        if str(nom) != "nan":
+            dataset.at[i, "Source"] += nom + ". "
+    dataset["Source"] += dataset["Definición"] + ". "
+    dataset["Source"] += dataset["Pregunta"] + "."
+
+    dataset["Target"] = dataset["Respuesta"]
+
+    return dataset["Source"].tolist(), dataset["Target"].tolist()
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "datasetfile", 
+        "dataset_file", 
         type = str,
         help = "El formato del archivo debe ser \'archivo.csv\'"
     )
 
     parser.add_argument(
-        "trainfile", 
+        "result_dir", 
         type = str,
-        help = "El formato del archivo debe ser \'archivo.csv\'"
-    )
-    parser.add_argument(
-        "validationfile", 
-        type = str,
-        help = "El formato del archivo debe ser \'archivo.csv\'"
+        help = "Debe ser un directorio existe"
     )
 
     parser.add_argument(
@@ -82,27 +68,37 @@ if __name__ == "__main__":
 
     try:
         args = parser.parse_args()
-        assert args.datasetfile.split('.')[-1] == "csv"
-        assert args.trainfile.split('.')[-1] == "csv"
-        assert args.validationfile.split('.')[-1] == "csv"
+        assert args.dataset_file.split('.')[-1] == "csv"
+        assert os.path.exists(args.result_dir)
         assert args.train_split > 0.0 and args.train_split < 1.0
     except:
         parser.print_help()
         sys.exit(0)
 
 
-    dataset = pd.read_csv(args.datasetfile)
+    dataset = pd.read_csv(args.dataset_file)
 
     train_dataset = obtenerTrainDataset(dataset, args.train_split)
 
     validation_dataset = obtenerValidationDataset(dataset, train_dataset)
 
-    train_dataset = generarContexto(train_dataset)
+    train_source, train_target = obtenerSourceTarget(train_dataset)
 
-    validation_dataset = generarContexto(validation_dataset)
+    val_source, val_target = obtenerSourceTarget(validation_dataset)
 
-    nombre_train_file = args.trainfile.split('.')[0] + "_" + str(args.train_split) + "." + args.trainfile.split('.')[-1]
-    train_dataset.to_csv(nombre_train_file)
+    dir = os.path.join(args.result_dir, f"split_{args.train_split}")
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    
 
-    nombre_vali_file = args.validationfile.split('.')[0] + "_" + str(args.train_split) + "." + args.validationfile.split('.')[-1]
-    validation_dataset.to_csv(nombre_vali_file)
+    with open(f"{dir}/train.source", 'w') as f:
+        f.writelines("%s\n" % l for l in train_source)
+
+    with open(f"{dir}/train.target", 'w') as f:
+        f.writelines("%s\n" % l for l in train_target)
+
+    with open(f"{dir}/val.source", 'w') as f:
+        f.writelines("%s\n" % l for l in val_source)
+
+    with open(f"{dir}/val.target", 'w') as f:
+        f.writelines("%s\n" % l for l in val_target)
