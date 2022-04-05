81a96a860541f44de122453a85da0f4910b1ef46
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-05 18:22:32 +0200

    Cambio

--- server_gpu/fine_tuning_3.py
+++ server_gpu/fine_tuning_3.py
@@ -214,6 +214,7 @@ tokenized_datasets = datasets.map(preprocess_function, batched=True)
 
 
 
+"""
 
 from transformers import DataCollatorForSeq2Seq
 
@@ -256,7 +257,6 @@ trainer = Seq2SeqTrainer(
 
 trainer.train()
 
-"""
 all_metrics = {}
 
 
@@ -271,7 +271,50 @@ if trainer.is_world_process_zero():
     handle_metrics("val", metrics, training_args.output_dir)
     all_metrics.update(metrics)
 
+
 """
 
+
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(tokenized_datasets["train"], shuffle=True, batch_size=8)
+eval_dataloader = DataLoader(tokenized_datasets["validation"], batch_size=8)
+
+
+from torch.optim import AdamW
+
+optimizer = AdamW(modelConver.parameters(), lr=5e-5)
+
+
+from transformers import get_scheduler
+
+num_epochs = 3
+num_training_steps = num_epochs * len(train_dataloader)
+lr_scheduler = get_scheduler(
+    name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
+)
+
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+modelConver.to(device)
+
+
+
+from tqdm.auto import tqdm
+
+progress_bar = tqdm(range(num_training_steps))
+
+modelConver.train()
+for epoch in range(num_epochs):
+    for batch in train_dataloader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = modelConver(**batch)
+        loss = outputs.loss
+        loss.backward()
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        progress_bar.update(1)
 
 
