fa3ee8658c7cc64b727a7ab224602f1002fe68fe
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-05 19:57:04 +0200

    Añadido dataset inglés

--- server_gpu/create_dataset_2.py
+++ server_gpu/create_dataset_2.py
@@ -6,6 +6,10 @@ import pandas as pd
 import sys
 import os
 
+import torch
+
+from transformers import AutoConfig, AutoTokenizer, MarianMTModel, TranslationPipeline
+
 
 def obtenerTrainDataset(dataset, train_split):
     train_dataset = dataset.sample(
@@ -43,8 +47,28 @@ def obtenerSourceTarget(dataset):
 
     return dataset["Source"].tolist(), dataset["Target"].tolist()
 
+def traducir(es_en_translator, source, target):
+    source_EN = []
+    target_EN = []
+
+    for s, t in zip(source, target):
+        s_EN = es_en_translator(
+            s
+        )[0]["translation_text"]
+
+        t_EN = es_en_translator(
+            t
+        )[0]["translation_text"]
+
+        source_EN.append(s_EN)
+        target_EN.append(t_EN)
+    
+    return source_EN, target_EN
+
 
 if __name__ == "__main__":
+
+    WORKDIR = "/mnt/homeGPU/mcarmona/"
 
     parser = argparse.ArgumentParser()
 
@@ -76,6 +100,34 @@ if __name__ == "__main__":
         sys.exit(0)
 
 
+    configTrans_ES_EN = AutoConfig.from_pretrained(
+        WORKDIR + "Helsinki-NLP/opus-mt-es-en/config.json"
+    )
+
+    tokenizerTrans_ES_EN = AutoTokenizer.from_pretrained(
+        WORKDIR + "Helsinki-NLP/opus-mt-es-en",
+        config=WORKDIR + "Helsinki-NLP/opus-mt-es-en/tokenizer_config.json",
+        use_fast=True
+    )
+
+    modelTrans_ES_EN = MarianMTModel.from_pretrained(
+        WORKDIR + "Helsinki-NLP/opus-mt-es-en",
+        from_tf=bool(".ckpt" in "Helsinki-NLP/opus-mt-es-en"),
+        config=configTrans_ES_EN,
+        torch_dtype=torch.float16
+    )
+
+    local_rank = int(os.getenv("LOCAL_RANK", "0"))
+
+    es_en_translator = TranslationPipeline(
+        model=modelTrans_ES_EN,
+        tokenizer=tokenizerTrans_ES_EN,
+        framework="pt",
+        device=local_rank
+    )
+
+
+
     dataset = pd.read_csv(args.dataset_file)
 
     train_dataset = obtenerTrainDataset(dataset, args.train_split)
@@ -84,16 +136,30 @@ if __name__ == "__main__":
 
     train_source, train_target = obtenerSourceTarget(train_dataset)
 
+    train_source_EN, train_target_EN = traducir(es_en_translator, train_source, train_target)
+
     train_aux = pd.DataFrame({
         "source": train_source,
         "target": train_target
     })
 
+    train_aux_EN = pd.DataFrame({
+        "source": train_source_EN,
+        "target": train_target_EN
+    })
+
     val_source, val_target = obtenerSourceTarget(validation_dataset)
+
+    val_source_EN, val_target_EN = traducir(es_en_translator, val_source, val_target)
 
     val_aux = pd.DataFrame({
         "source": val_source,
         "target": val_target
+    })
+
+    val_aux_EN = pd.DataFrame({
+        "source": val_source_EN,
+        "target": val_target_EN
     })
 
     dir = os.path.join(args.result_dir, f"split_{args.train_split}")
@@ -101,4 +167,6 @@ if __name__ == "__main__":
         os.mkdir(dir)
     
     train_aux.to_csv(f"{dir}/train.csv")
+    train_aux_EN.to_csv(f"{dir}/train_EN.csv")
     val_aux.to_csv(f"{dir}/val.csv")
+    val_aux_EN.to_csv(f"{dir}/val_EN.csv")
