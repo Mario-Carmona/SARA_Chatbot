89de64830caface08bfa3bf33e55b6ae42a89fba
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-06 00:48:30 +0200

    Cambio

--- server_gpu/fine_tuning_3.py
+++ server_gpu/fine_tuning_3.py
@@ -323,6 +323,17 @@ for epoch in range(num_epochs):
 
 
 
+metric = load_metric("accuracy")
+modelConver.eval()
+for batch in eval_dataloader:
+    batch = {k: v.to(device) for k, v in batch.items()}
+    with torch.no_grad():
+        outputs = modelConver(**batch)
 
+    logits = outputs.logits
+    predictions = torch.argmax(logits, dim=-1)
+    metric.add_batch(predictions=predictions, references=batch["labels"])
+
+metric.compute()
 
 
