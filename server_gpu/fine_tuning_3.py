f46180dcbb2ffe6334be4eda23f036b84991a12e
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-05 16:42:06 +0200

    Cambio

--- server_gpu/fine_tuning_3.py
+++ server_gpu/fine_tuning_3.py
@@ -257,6 +257,33 @@ modelConver.to(device)
 os.system("nvidia-smi")
 
 
+
+from tqdm.auto import tqdm
+
+progress_bar = tqdm(range(num_training_steps))
+
+modelConver.train()
+for epoch in range(num_epochs):
+    for batch in train_dataloader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = modelConver(**batch)
+        loss = outputs.loss
+        loss.backward()
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        progress_bar.update(1)
+
+
+
+
+
+
+
+
+
+
 """
 # TODO: Once the fix lands in a Datasets release, remove the _local here and the squad_v2_local folder.
 metric = load_metric("bleu")
