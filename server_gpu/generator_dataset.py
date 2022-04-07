210bc59afc756c859a50784401fccd1e9adc124e
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-07 02:25:39 +0200

    Cambio

--- server_gpu/generator_dataset.py
+++ server_gpu/generator_dataset.py
@@ -43,7 +43,7 @@ def summarization(dataset, configSum, tokenizerSum, modelSum, device):
         batch = tokenizerSum(i, truncation=True, padding="longest", return_tensors="pt")
         
         if(batch['input_ids'].shape[1] <= 50):
-            frases = i.split('. ')
+            frases = i.split(sep='. ')
             for pos in range(len(frases)-1):
                 frases[pos] += "."
             text += frases
