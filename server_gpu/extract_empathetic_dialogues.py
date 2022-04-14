a7b8e4e68bb8615cb1fd9c3c48fb5fa680b34948
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-14 13:14:44 +0200

    Pruebas nuevos scripts

---
+++ server_gpu/extract_empathetic_dialogues.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import argparse
+from datasets import load_dataset
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'list_sentiment', 
+        type=list, 
+        help=''
+    )
+    args = parser.parse_args()
+
+
+    dataset = load_dataset("empathetic_dialogues")
+
+    print(dataset)
+
+
---
+++ server_gpu/reorder_dataset.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import argparse
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'dataset_file', 
+        type=str, 
+        help=''
+    )
+    args = parser.parse_args()
+
+
+    # Lectura del dataset
+    dataset = pd.read_csv(args.dataset_file)
+
+    # Eliminación de una columna que se añade al guardar el archivo CSV
+    dataset = dataset.drop(columns=["Unnamed: 0"])
+
+    # Ordenar el dataset en base a la columna Topic
+    dataset = dataset.sort_values('Topic')
+
+    # Guardar el dataset tras su ordenación
+    dataset.to_csv(args.dataset_file)
