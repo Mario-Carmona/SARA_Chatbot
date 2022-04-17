e1c3a2f307198f7cc6a458f47abd16f39ec8132c
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-17 19:51:28 +0200

    Cambio

--- server_gpu/fine_tuning_4.py
+++ server_gpu/fine_tuning_4.py
@@ -24,13 +24,6 @@ from transformers import EvalPrediction, Seq2SeqTrainer
 from transformers.trainer_utils import is_main_process, EvaluationStrategy
 from transformers.training_args import ParallelMode
 
-from utils import (
-    Seq2SeqDataCollator,
-    Seq2SeqDataset,
-    check_output_dir,
-    save_json,
-)
-
 import torch
 
 from sacrebleu import corpus_bleu
@@ -39,6 +32,28 @@ import numpy as np
 
 from torch.utils.data import DataLoader
 
+
+
+def check_output_dir(args, expected_items=0):
+    """
+    Checks whether to bail out if output_dir already exists and has more than expected_items in it
+    `args`: needs to have the following attributes of `args`:
+      - output_dir
+      - do_train
+      - overwrite_output_dir
+    `expected_items`: normally 0 (default) - i.e. empty dir, but in some cases a few files are expected (e.g. recovery from OOM)
+    """
+    if (
+        os.path.exists(args.output_dir)
+        and len(os.listdir(args.output_dir)) > expected_items
+        and args.do_train
+        and not args.overwrite_output_dir
+    ):
+        raise ValueError(
+            f"Output directory ({args.output_dir}) already exists and "
+            f"has {len(os.listdir(args.output_dir))} items in it (expected {expected_items} items). "
+            "Use --overwrite_output_dir to overcome."
+        )
 
 
 
