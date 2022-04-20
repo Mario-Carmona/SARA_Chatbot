923bd239e51abb41201700bce19f757696efbe94
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-21 00:34:10 +0200

    Cambio

--- server_gpu/DialoGPT-master/prueba.py
+++ server_gpu/DialoGPT-master/prueba.py
@@ -19,7 +19,7 @@ tokenizer = GPT2Tokenizer.from_pretrained(
 )
 """
 
-tokenizer = AutoTokenizer.from_pretrained(model_path)
+tokenizer = GPT2Tokenizer.from_pretrained(model_path)
 
 config = GPT2Config.from_json_file(
     join(model_path, 'config.json')
@@ -31,7 +31,9 @@ args = {
     "fp16": True
 }
 
-model = AutoModelForCausalLM.from_pretrained(model_path, config=config)
+
+model = GPT2LMHeadModel.from_pretrained(model_path)
+
 
 """
 model = load_model(GPT2LMHeadModel(config), join(model_path, 'pytorch_model.bin'),
@@ -42,19 +44,20 @@ model = load_model(GPT2LMHeadModel(config), join(model_path, 'pytorch_model.bin'
 
 
 
-new_user_input_ids = tokenizer.encode(input(">> User:") + tokenizer.eos_token, return_tensors='pt')
+new_user_input_ids = tokenizer.encode(input(">> User:") + "<|endoftext|>")
 # append the new user input tokens to the chat history
 bot_input_ids = new_user_input_ids
+
+print(bot_input_ids)
+
 # generated a response while limiting the total chat history to 1000 tokens, 
-chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+chat_history_ids = model(**bot_input_ids, max_length=1000, pad_token_id="<|endoftext|>")
+
+print(chat_history_ids)
+"""
 # pretty print last ouput tokens from bot
 print("DialoGPT_MWOZ_Bot: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
-
-
-
-
-
-
+"""
 
 
 
---
+++ server_gpu/DialoGPT-master/prueba_2.py
@@ -0,0 +1,29 @@
+
+from lsp_model import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config
+from gpt2_training.train_utils import load_model
+import torch
+from os.path import join
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+model_path = "/mnt/homeGPU/mcarmona/tosin/dialogpt_mwoz"
+
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path)
+
+
+
+# encode the new user input, add the eos_token and return a tensor in Pytorch
+new_user_input_ids = tokenizer.encode(input(">> User:") + tokenizer.eos_token, return_tensors='pt')
+# append the new user input tokens to the chat history
+bot_input_ids = new_user_input_ids
+# generated a response while limiting the total chat history to 1000 tokens, 
+chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+# pretty print last ouput tokens from bot
+print("DialoGPT_MWOZ_Bot: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
+
+
+
+
+
+
