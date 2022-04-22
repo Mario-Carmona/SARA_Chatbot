4c33c0dcba3dcb2b952019810bb63c3ea8404a63
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-22 17:31:46 +0200

    Cambio

--- app/static/js/capture_image.js
+++ app/static/js/capture_image.js
@@ -98,8 +98,11 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var video = document.getElementById('video');
+var relacion = 1.33;
 
 // Trigger photo take
 document.getElementById("snap").addEventListener("click", function() {
-    context.drawImage(video, 0, 0, video.clientWidth, video.clientHeight);
+    var videoHeight = relacion * video.clientWidth;
+    var inicioHeight = (video.clientHeight - videoHeight) / 2;
+    context.drawImage(video, 0, inicioHeight, video.clientWidth, videoHeight);
 });
