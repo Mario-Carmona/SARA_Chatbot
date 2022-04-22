a06664d4cb0911c24660d2821f6192ee37afecef
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-22 16:50:47 +0200

    Prueba

--- app/static/js/capture_image.js
+++ app/static/js/capture_image.js
@@ -23,12 +23,14 @@ window.addEventListener('resize', () => {
 
 /********************************/
 
+/*
 'use strict';
 
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const snap = document.getElementById("snap");
 const errorMsgElement = document.querySelector('span#errorMsg');
+*/
 
 /*
 navigator.mediaDevices.getUserMedia({ video: { width: video.clientWidth, height: video.clientHeight } }).then(function(stream) {
@@ -42,6 +44,7 @@ snap.addEventListener("click", function() {
 });
 */
 
+/*
 const constraints = {
     video: {
         width: video.clientWidth,
@@ -74,4 +77,29 @@ init();
 var context = canvas.getContext('2d');
 snap.addEventListener("click", function() {
     context.drawImage(stream, 0, 0);
+});
+*/
+
+
+
+// Grab elements, create settings, etc.
+var video = document.getElementById('video');
+
+// Get access to the camera!
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function() {
+    context.drawImage(video, 0, 0, 640, 480);
 });
