bb7b7b1d7ce478a402836a351116fcdb1aea54bd
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-22 16:54:15 +0200

    Cambio

--- app/static/js/capture_image.js
+++ app/static/js/capture_image.js
@@ -95,6 +95,7 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     });
 }
 
+/*
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var video = document.getElementById('video');
@@ -103,3 +104,4 @@ var video = document.getElementById('video');
 document.getElementById("snap").addEventListener("click", function() {
     context.drawImage(video, 0, 0, 640, 480);
 });
+*/
