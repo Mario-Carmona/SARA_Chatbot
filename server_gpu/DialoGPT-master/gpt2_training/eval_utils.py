d76508170ad631ade13c1d2aeff71dde50be7fa4
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-30 20:20:07 +0200

    Cambio

--- server_gpu/DialoGPT-master/gpt2_training/eval_utils.py
+++ server_gpu/DialoGPT-master/gpt2_training/eval_utils.py
@@ -62,7 +62,9 @@ def eval_model_loss(model, tokenizer, eval_dataloader, epoch_id, args):
     metric = load_metric("accuracy")
     with torch.no_grad():
         for step, batch in enumerate(eval_dataloader):
+            print(batch)
             batch = tuple(t.to(args.device) for t in batch)
+            print(batch)
             input_ids, position_ids, token_ids, label_ids, src_len, _ = batch
             if args.no_token_id:
                 token_ids = None
