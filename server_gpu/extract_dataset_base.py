55be39ff2882dae11cc2e13a3206c26fe6455dc8
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-30 17:40:45 +0200

    Cambio

--- server_gpu/extract_dataset_base.py
+++ server_gpu/extract_dataset_base.py
@@ -7,26 +7,6 @@ import random
 from datasets import load_dataset
 
 
-def extract_conv_ai_2_dataset(train_split: float):
-    dataset = load_dataset("conv_ai_2", revision="master")
-
-    dialog = dataset["train"]["dialog"]
-
-    conversaciones = []
-
-    for conver in dialog:
-        len_dialog = len(conver) if len(conver)%2 == 0 else len(conver)-1
-        for j in range(0,len_dialog,2):
-            entry = conver[j]["text"]
-            response = conver[j+1]["text"]
-            conversaciones.append(f"{entry}\t{response}")
-    
-    random.shuffle(conversaciones)
-
-    div = int(len(conversaciones) * train_split)
-
-    return conversaciones[:div], conversaciones[div:]
-
 
 def extract_daily_dialog_dataset(train_split: float):
     dataset = load_dataset("daily_dialog", revision="master")
@@ -100,14 +80,12 @@ if __name__ == "__main__":
         sys.exit(0)
 
 
-    conv_ai_2_train, conv_ai_2_valid = extract_conv_ai_2_dataset(args.train_split)
-
     daily_dialog_train, daily_dialog_valid = extract_daily_dialog_dataset(args.train_split)
 
     multi_woz_v22_train, multi_woz_v22_valid = extract_multi_woz_v22_dataset(args.train_split)
 
-    dataset_train = conv_ai_2_train + daily_dialog_train + multi_woz_v22_train
-    dataset_valid = conv_ai_2_valid + daily_dialog_valid + multi_woz_v22_valid
+    dataset_train = daily_dialog_train + multi_woz_v22_train
+    dataset_valid = daily_dialog_valid + multi_woz_v22_valid
 
     random.shuffle(dataset_train)
     random.shuffle(dataset_valid)
