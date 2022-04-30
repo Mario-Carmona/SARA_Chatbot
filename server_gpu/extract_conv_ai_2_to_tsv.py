1296fae75d11f15e2523b301e9d69979744c990e
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-04-30 13:48:51 +0200

    Cambio

---
+++ server_gpu/extract_conv_ai_2_to_tsv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from datasets import load_dataset
+
+
+
+
+
+
+if __name__ == "__main__":
+    dataset = load_dataset("conv_ai_2")
+    print(dataset)
+
+
