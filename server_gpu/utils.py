69e3a2ee368904e414d35d6e793ce6c12bca2e01
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-05-04 00:49:19 +0200

    Cambio

--- server_gpu/utils.py
+++ server_gpu/utils.py
@@ -3,7 +3,10 @@ def save_csv(dataset, filepath):
     text = []
     text.append(','.join(list(dataset.columns.values)))
     for i in range(dataset.shape[0]):
-        line = [f"\"{elem}\"" for elem in dataset.iloc[i]]
+        line = []
+        for elem in dataset.iloc[i]:
+            new_elem = elem.replace("\"", "'")
+            line.append(f"\"{new_elem}\"")
         text.append(','.join(line))
     with open(filepath, 'w') as f:
         f.write('\n'.join(text))
