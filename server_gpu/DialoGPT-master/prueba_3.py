6f149f9b1b56a4076ad9f37cda81d1450f416fb4
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-05-11 18:57:32 +0200

    Cambio

--- server_gpu/DialoGPT-master/prueba_3.py
+++ server_gpu/DialoGPT-master/prueba_3.py
@@ -8,7 +8,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
 
-mname = "/mnt/homeGPU/mcarmona/facebook/blenderbot-1B-distill"
+mname = "/mnt/homeGPU/mcarmona/facebook/blenderbot-400M-distill"
 model = BlenderbotForConditionalGeneration.from_pretrained(mname)
 tokenizer = BlenderbotTokenizer.from_pretrained(mname)
 
@@ -20,8 +20,10 @@ reply_ids = model.generate(**inputs)
 print(tokenizer.batch_decode(reply_ids))
 """
 
-for step in range(5):
-    new_user_input_ids = tokenizer.encode(input(">> User: ") + tokenizer.eos_token, return_tensors='pt')
+for step in range(1):
+    entry = input(">> User: ") + tokenizer.eos_token
+    print(entry)
+    new_user_input_ids = tokenizer.encode(entry, return_tensors='pt')
     if step > 0:
       bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], axis=-1)  
     else:
@@ -29,5 +31,6 @@ for step in range(5):
 
     chat_history_ids = model.generate(bot_input_ids, max_length=1000, max_time=3.0, pad_token_id=tokenizer.eos_token_id)
 
-    print("Blendetbot: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
+    output = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    print("Blendetbot: {}".format(output))
 
