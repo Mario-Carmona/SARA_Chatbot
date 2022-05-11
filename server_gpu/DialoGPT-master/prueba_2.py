2e9d589a4c4b50a8491776001614a3a8f2db35ec
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-05-11 12:10:28 +0200

    Cambio

--- server_gpu/DialoGPT-master/prueba_2.py
+++ server_gpu/DialoGPT-master/prueba_2.py
@@ -6,7 +6,7 @@ from os.path import join
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
-model_path = "/mnt/homeGPU/mcarmona/tosin/dialogpt_mwoz"
+model_path = "/mnt/homeGPU/mcarmona/result_finetuning/v2_adult"
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForCausalLM.from_pretrained(model_path)
@@ -19,6 +19,7 @@ new_user_input_ids = tokenizer.encode(input(">> User:") + tokenizer.eos_token, r
 bot_input_ids = new_user_input_ids
 # generated a response while limiting the total chat history to 1000 tokens, 
 chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+print(chat_history_ids)
 # pretty print last ouput tokens from bot
 print("DialoGPT_MWOZ_Bot: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
 
