b8317663102f85346e0c63beb8913615f1b7a313
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-05-14 20:32:03 +0200

    Cambio

--- server_gpu/dataclass/generate_arguments.py
+++ server_gpu/dataclass/generate_arguments.py
@@ -2,9 +2,35 @@
 from dataclasses import dataclass, field
 from typing import Optional
 
+from pkg_resources import require
+
 
 @dataclass
 class GenerateArguments:
+    do_sample: bool = field(
+        metadata={
+            "help": ""
+        },
+        required=False
+    )
+    temperature: float = field(
+        metadata={
+            "help": ""
+        },
+        required=False
+    )
+    top_p: float = field(
+        metadata={
+            "help": ""
+        },
+        required=False
+    )
+    max_time: float = field(
+        metadata={
+            "help": ""
+        },
+        required=False
+    )
     max_length: int = field(
         metadata={
             "help": ""
@@ -14,4 +40,10 @@ class GenerateArguments:
         metadata={
             "help": ""
         }
+    )
+    use_cache: bool = field(
+        metadata={
+            "help": ""
+        },
+        required=False
     )
