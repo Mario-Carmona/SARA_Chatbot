cb89130d68d149e926d110783bc136ad4d089c30
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-05-15 10:29:25 +0200

    Cambio

--- server_gpu/main.py
+++ server_gpu/main.py
@@ -299,6 +299,8 @@ def send_public_URL():
     pubkey = KEY_SERVER_GPU.publickey()
     pubkey_string = pubkey.exportKey("PEM")
     print(type(pubkey_string))
+    print(pubkey_string)
+    print(str(pubkey_string))
     input("--->")
     
     headers = {'content-type': 'application/json'}
