6b5da73955cedaf047cc070e760f06d14321ec33
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-05-17 14:05:20 +0200

    Cambio

--- app/static/js/capture_image.js
+++ app/static/js/capture_image.js
@@ -74,7 +74,7 @@ function getBase64(file) {
         Http.send(JSON.stringify(data));
 
         var loading = document.getElementById('loading');
-        loading.fadeIn("slow");
+        loading.classList.toggle('active');
     };
 }
 
