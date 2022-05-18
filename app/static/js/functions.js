b88dd4f833b82fd4d4db3d5e064c607192ab7565
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-05-18 13:27:05 +0200

    Cambio

--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -22,60 +22,58 @@ function openChatbot(canal) {
 
     Http.send();
 
+    var response = "";
+
     Http.onreadystatechange = (e) => {
-        var response = Http.responseText;
-
-        console.log(Http);
-
-        console.log(response);
-
-        if (response != 'Server GPU ON') {
-            Swal.fire({
-                icon: 'error',
-                title: 'Error de conexión...',
-                text: 'El servidor GPU no está disponible en este momento.'
-            });
-        } else {
-            Swal.fire({
-                title: '¿Quiere deducir su edad mediante una foto?',
-                text: "¡No podrás revertir tu decisión!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#00FF24',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Si',
-                cancelButtonText: 'No'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    openURL('./capture_image', canal);
-                } else {
-                    Swal.fire({
-                        title: '¿Cuál es tu edad?',
-                        text: 'Debes indicar cual de los siguientes rangos de edad se asocia más con tu edad actual.',
-                        showCancelButton: true,
-                        confirmButtonColor: '#2563B9',
-                        cancelButtonColor: '#EBB241',
-                        confirmButtonText: 'Adulto',
-                        cancelButtonText: 'Niño'
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            if (canal == "web") {
-                                openURL('./' + document.getElementById('web_adult').innerText);
-                            } else if (canal == "telegram") {
-                                openURL(document.getElementById('telegram_adult').innerText);
-                            }
-                        } else {
-                            if (canal == "web") {
-                                openURL('./' + document.getElementById('web_child').innerText);
-                            } else if (canal == "telegram") {
-                                openURL(document.getElementById('telegram_child').innerText);
-                            }
-                        }
-                    });
-                }
-            });
-        }
+        response = Http.responseText;
     };
+
+    if (response != 'Server GPU ON') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error de conexión...',
+            text: 'El servidor GPU no está disponible en este momento.'
+        });
+    } else {
+        Swal.fire({
+            title: '¿Quiere deducir su edad mediante una foto?',
+            text: "¡No podrás revertir tu decisión!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#00FF24',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si',
+            cancelButtonText: 'No'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                openURL('./capture_image', canal);
+            } else {
+                Swal.fire({
+                    title: '¿Cuál es tu edad?',
+                    text: 'Debes indicar cual de los siguientes rangos de edad se asocia más con tu edad actual.',
+                    showCancelButton: true,
+                    confirmButtonColor: '#2563B9',
+                    cancelButtonColor: '#EBB241',
+                    confirmButtonText: 'Adulto',
+                    cancelButtonText: 'Niño'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        if (canal == "web") {
+                            openURL('./' + document.getElementById('web_adult').innerText);
+                        } else if (canal == "telegram") {
+                            openURL(document.getElementById('telegram_adult').innerText);
+                        }
+                    } else {
+                        if (canal == "web") {
+                            openURL('./' + document.getElementById('web_child').innerText);
+                        } else if (canal == "telegram") {
+                            openURL(document.getElementById('telegram_child').innerText);
+                        }
+                    }
+                });
+            }
+        });
+    }
 }
 
 
