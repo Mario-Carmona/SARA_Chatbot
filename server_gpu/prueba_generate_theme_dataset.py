1ee9c37be55f7feeb7cb2bcda620addfd0f28406
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2022-06-11 00:36:39 +0200

    Cambio 11

--- server_gpu/prueba_generate_theme_dataset.py
+++ server_gpu/prueba_generate_theme_dataset.py
@@ -268,8 +268,8 @@ def generarResumenes(question, answer):
     batch_question = tokenizerSum(question, padding="longest", return_tensors="pt")
 
     # El máximo del modelo de resumir es 512, sino da errores
-    if len(batch_question) > 512:
-        batch_question = batch_question[:512]
+    #if len(batch_question) > 512:
+    #    batch_question = batch_question[:512]
     
     translated_question = modelSum.generate(**batch_question, max_length=generate_args.max_length_summary, num_beams=generate_args.num_beams_summary, num_return_sequences=generate_args.num_beams_summary)
     tgt_text_question = tokenizerSum.batch_decode(translated_question, skip_special_tokens=True)
@@ -284,8 +284,8 @@ def generarResumenes(question, answer):
     # Obtener los tokens del texto
     batch_answer = tokenizerSum(answer, padding="longest", return_tensors="pt")
 
-    if len(batch_answer) > 512:
-        batch_answer = batch_answer[:512]
+    #if len(batch_answer) > 512:
+    #    batch_answer = batch_answer[:512]
     
     translated_answer = modelSum.generate(**batch_answer, max_length=generate_args.max_length_summary, num_beams=generate_args.num_beams_summary, num_return_sequences=generate_args.num_beams_summary)
     tgt_text_answer = tokenizerSum.batch_decode(translated_answer, skip_special_tokens=True)
